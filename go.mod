6b100508ff1a558e45f131ca44d41ae00be42e8e
Bhupendra66 <bhupendrapundir6@email.com>
2025-10-21 20:24:30 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/BHUPE01/GO-hello-web
+
+go 1.24.5
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, world")
+	})
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintln(w, "OK")
+	})
+
+	fmt.Println("Server starting on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
