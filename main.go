076afb8cf7b6e439b45815308b090efe37cc47cd
Bhupendra66 <bhupendrapundir6@email.com>
2025-10-22 19:04:05 +0530

    Add new code version

--- main.go
+++ main.go
@@ -4,18 +4,25 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello, world")
-	})
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	path := strings.TrimPrefix(r.URL.Path, "/hello/")
+	if path == "" {
+		fmt.Fprintln(w, "Hello, Stranger")
+		return
+	}
+	fmt.Fprintf(w, "Hello, %s!\n", path)
+}
 
+func main() {
+	http.HandleFunc("/hello/", helloHandler)
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprintln(w, "OK")
 	})
 
-	fmt.Println("Server starting on :8080")
+	log.Println("Server on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
